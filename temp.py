7e19fe4107f6c0bcbb711ee46a5ec60239e1a004
bodelha <bodelha@gmail.com>
2020-09-14 01:42:47 -0300

    need to correct the functions adjust

--- temp.py
+++ temp.py
@@ -1,50 +1,112 @@
-import numpy as np
-
 class Node:
     def __init__ (self, valor):
         self.valor = valor
+        self.prev = None
         self.next = None
-    
-    def mostra_no (self):
-        print (self.valor)
 
-class LinkedList:
+    def show (self):
+        print (
+            f"Valor = {self.valor}", 
+            f"Anterior = {self.prev}", 
+            f"Próximo = {self.next}", 
+            sep='\n'
+            )
+    
+
+class DoublyLinkedList:
     def __init__ (self):
         self.head = None
+        self.tail = None
+    
+    def __lista_vazia (self):
+        if not self.head and not self.tail:
+            print ("Vazia")
+            return True
+        return False
+
+    def __adjust_left (self, new, old):
+        if old:
+            old.prev = new
+        new.next = old
+    
+    def __adjust_right (self, old, new):
+        if old:
+            old.next = new
+        new.prev = old
 
     def insere_inicio (self, valor):
         new = Node (valor)
-        new.next = self.head
+        if self.__lista_vazia ():
+            self.tail = new
+        else:
+            self =  self.__adjust_left (new, self.head)
         self.head = new
+
+    def insere_final (self, valor):
+        new = Node (valor)
+        if self.__lista_vazia ():
+            self.head = new
+        else:
+            self.__adjust_right (self.head, new)
+        self.tail = new
+
+    def exclui_inicio (self):
+        if self.__lista_vazia():
+            return
+        self.__adjust_right (None, self.head.next)
+        self.head = self.head.next
+
+    def exclui_final (self):
+        if self.__lista_vazia:
+            return
+        self.__adjust_left (self.tail.prev, None)
+        self.tail = self.tail.prev
+
+    def exclui_valor (self, valor):
+        if self.head.valor == valor:
+            return exclui_inicio ()
+        if self.tail.valor == valor:
+            return exclui_final ()
+        atual1 = self.head.next
+        atual2 = self.tail.prev
+        while atual1.valor != valor or atual2.valor != valor:
+            if atual1 == atual2 or atual1 == atual2.prev:
+                print ('Não está na lista')
+                return
+            atual1 = atual1.next
+            atual2 = atual2.prev
+        atual = atual1 if atual1.valor == valor else atual2
+        self.__adjust_left (atual.prev, atual.next)
 
     def mostrar (self):
         atual = self.head
         while atual:
-            atual.mostra_no()
+            atual.show()
             atual = atual.next
+        
+lista = DoublyLinkedList()
+lista.insere_final(3)
+lista.insere_final(4)
+lista.insere_inicio(9)
+lista.insere_final(5)
+lista.insere_inicio(1)
+lista.insere_final(7)
+lista.insere_final(-7)
+lista.insere_inicio(-2)
+lista.insere_final(6)
+lista.mostrar()
+lista.exclui_inicio()
+lista.mostrar()
+lista.exclui_final()
+lista.mostrar()
+lista.exclui_valor(9)
+lista.mostrar()
+lista.exclui_valor(9)
+lista.mostrar()
+lista.exclui_final()
+lista.exclui_final()
+lista.exclui_inicio()
+lista.exclui_inicio()
+lista.exclui_inicio()
+lista.exclui_inicio()
 
-    def exclui_inicio (self):
-        temp = self.head
-        self.head = self.head.next 
-        if self.head:
-            return temp
-        else:
-            return self.head
-
-lista = LinkedList ()
-lista.insere_inicio(8)
-lista.insere_inicio(10)
-lista.insere_inicio(3)
-lista.mostrar()
-lista.insere_inicio(6)
-lista.mostrar()
-lista.insere_inicio(-7)
-lista.mostrar()
-lista.exclui_inicio()
-lista.mostrar()
-lista.exclui_inicio()
-lista.exclui_inicio()
-lista.exclui_inicio()
-lista.mostrar()
-lista.exclui_inicio()
-lista.mostrar()
